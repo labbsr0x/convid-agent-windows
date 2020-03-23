0deb4b4842154d4402a423b6f9f17580522ffc4a
tiagostutz <tiagostutz@gmail.com>
2020-03-23 13:49:42 -0300

    fixing rdp call

--- enrollment-application/frontend/src/App.js
+++ enrollment-application/frontend/src/App.js
@@ -24,9 +24,6 @@ function AppModel() {
 
   const enroll = (address, accountId) => {
     setBusy(true)
-    window.wails.on("ConnectionSucceed", _ => {
-      console.log("===>>>> RETRUINGGG!!!!!");
-    })
     window.backend.doRegister(address, accountId).then(ret => {
       if (!ret.error) {
         setMachineId(ret.machineId)
